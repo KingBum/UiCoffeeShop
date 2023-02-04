a9a08b3b372f5a17f10380ad7e58d07597d0eb69
Dat51034 <dat51034@donga.edu.vn>
2023-02-04 19:38:20 +0700

    2312

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    createProxyMiddleware(["/api", , "/otherApi"], { target: "http://localhost:7000" })
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:7000',
+      changeOrigin: true,
+    })
   );
 };
