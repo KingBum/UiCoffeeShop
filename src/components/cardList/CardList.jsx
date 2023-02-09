e80c9b51e981d20947000300e27f502cf8df079b
Dat51034 <dat51034@donga.edu.vn>
2023-02-09 11:06:59 +0700

    asdassdsadasddsd

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -9,7 +9,7 @@ export default function CardList() {
     useEffect(() => {
         const fetchPosts = async () => {
           const res = await axios.get("/products/")
-          const covert = JSON.parse(res.data)
+          const covert = res.data.json()
           setMenu(covert.sort((p1,p2) => {
             return new Date(p2.createdAt) - new Date(p1.createdAt)
           }))
